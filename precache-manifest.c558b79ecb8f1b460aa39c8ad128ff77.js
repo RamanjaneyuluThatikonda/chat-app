2666ef5e20ad65fd0febf9d227b3f6fb42e53357
Ramanjaneyulu Thatikonda <Ramu@gmail.com>
2021-06-18 21:07:38 +0530

    Updates

---
+++ precache-manifest.c558b79ecb8f1b460aa39c8ad128ff77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abde9e8ba5316f0aefa162fa5a6068dd",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "783f4d233d7a07ab0ef5",
+    "url": "/chat-app/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "615937843efce533f6e9",
+    "url": "/chat-app/static/js/2.cf8c7879.chunk.js"
+  },
+  {
+    "revision": "783f4d233d7a07ab0ef5",
+    "url": "/chat-app/static/js/main.af5b167c.chunk.js"
+  },
+  {
+    "revision": "ecb668af3d80c049442a",
+    "url": "/chat-app/static/js/runtime~main.1d27bffe.js"
+  }
+]);
